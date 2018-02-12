33c34bd4049a5d792d0f9dd1696952b75bd62086
OmerFlame <renginald.bagel@gmail.com>
2018-02-12 10:24:11 +0200

    Added a "phone-login" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PhoneLoginComponent } from './phone-login/phone-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneLoginComponent
   ],
   imports: [
     BrowserModule
