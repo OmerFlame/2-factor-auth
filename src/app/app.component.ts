a8d7e5263068ce4d27ea3b3d9713aa99403678d4
OmerFlame <renginald.bagel@gmail.com>
2018-02-13 00:41:59 +0200

    Fixing things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import speakeasy from 'speakeasy';
+import QRCode from 'qrcode';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  secret: any;
+
+  GenerateSecretKey() {
+    this.secret = speakeasy.generateSecret({length: 20});
+    console.log(this.secret.base32);
+  }
+
+  GenerateQR() {
+    QRCode.toDataURL(this.secret.otpauth_url, function(err, image_data) {
+      console.log(image_data);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatInputModule, MatFormField } from '@angular/material';
-
+import { MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { PhoneLoginComponent } from './phone-login/phone-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhoneLoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatFormField
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
