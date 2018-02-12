cb1507d69783a9944c61469d627d181ff5113268
OmerFlame <renginald.bagel@gmail.com>
2018-02-12 10:38:16 +0200

    Added a login callback function and a captcha verifier

--- src/app/phone-login/phone-login.component.ts
+++ src/app/phone-login/phone-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowService } from '../window.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-phone-login',
@@ -6,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone-login.component.css']
 })
 export class PhoneLoginComponent implements OnInit {
+  windowRef: any;
+  phoneNumber = new PhoneNumber()
+  verificationCode: string;
+
+  user: any;
+
+  constructor(private win: WindowService) {  }
+
+  ngOnInit() {
+    this.windowRef = this.win.windowRef;
+    this.windowRef.racaptchaVerifier.render();
+  }
+
+
+  sendLoginCode() {
+    const appVerifier = this.windowRef.recaptchaVerifier;
+
+    const num = this.phoneNumber.e164;
+
+    firebase.auth().signInWithPhoneNumber(num, appVerifier)
+    .then(result => {
+      this.windowRef.confirmationResult = result;
+    })
+    .catch( error => console.log(error) );
+  }
+
+  verifyLoginCode() {
+    this.windowRef.confirmationResult.confirm(this.verificationCode).then( result => {
+      this.user = result.user;
+    }).catch( error => console.log(error, "Incorrect code entered?"));
+  }
+
+
   country: string;
   area: string;
   prefix: string;
